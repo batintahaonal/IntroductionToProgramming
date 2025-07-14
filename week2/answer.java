0678b237b5bc3502943228ab7f172fa227e094c7
Batın Taha Önal <153363158+batintahaonal@users.noreply.github.com>
2025-07-14 14:42:08 +0300

    Add files via upload

---
+++ week2/answer.java
@@ -0,0 +1,13 @@
+package ders1;
+
+public class BMI {
+public static void main(String[]args) {
+int weight=69;
+double height=1.84;
+double BMI;
+BMI=((weight)/(height*height));
+System.out.println("My BMI:"+BMI);
+}
+
+}
+/*Batin Taha Onal 220315086  */
