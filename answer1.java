8bb8b11c8f31a3f85e5ea6499b0ae0a2be560f91
Batın Taha Önal <153363158+batintahaonal@users.noreply.github.com>
2025-07-14 14:22:11 +0300

    Add files via upload

---
+++ answer1.java
@@ -0,0 +1,17 @@
+package ders1;
+
+
+/* name surname:Batin Taha Önal 
+   Student ID:220315086  */
+
+
+
+
+public class Question1 {
+public static void main(String[]args) {
+System.out.println("it's my first project");
+System.out.println("hello world");
+System.out.println("I love Java");
+}
+
+}
---
+++ answer2.java
@@ -0,0 +1,20 @@
+package ders1;
+
+
+/* name surname:Batin Taha Önal 
+   Student ID:220315086  */
+
+
+
+
+public class Question2 {
+public static void main(String[]args) {
+System.out.println("I love Java ");
+System.out.println("I love Java ");
+System.out.println("I love Java ");
+System.out.println("I love Java ");
+System.out.println("I love Java ");
+}
+
+}
+
